545cf5d1ca7e49349c631aaea9efc7ef395efae2
kohey <very.tasty.konjac@gmail.com>
2021-10-11 04:06:38 +0900

    vueセットアップ

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  purge: [
+    './public/index.html',
+    './src/**/*.{vue,js,ts,jsx,tsx}',
+  ],
+  darkMode: false,
+  theme: {
+    extend: {},
+  },
+  variants: {},
+  plugins: [],
+}

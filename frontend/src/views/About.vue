60b7e9453e25b020483b50a5989752102c71b2a0
kohey <very.tasty.konjac@gmail.com>
2021-10-19 05:20:18 +0900

    /api/aboutのテスト

--- frontend/src/views/About.vue
+++ frontend/src/views/About.vue
@@ -1,5 +1,24 @@
+<script setup lang="ts">
+import { onMounted, ref } from 'vue';
+import axios from 'axios';
+
+const about = ref<string | unknown>('');
+onMounted(async () => {
+  try {
+    const aboutResponse = await axios.get('/api/about');
+    about.value = aboutResponse.data;
+  } catch (error) {
+    if (axios.isAxiosError(error)) {
+      console.dir(error);
+    } else {
+      console.dir(error);
+    }
+  }
+});
+</script>
+
 <template>
   <div class="about">
-    About
+    {{ about }}
   </div>
 </template>
